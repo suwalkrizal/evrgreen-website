a2668eab9d88dfd8cf1d2c9e92891b7c0234774a
Diya <shresthadiya59@gmail.com>
2024-04-23 11:27:04 +0545

    add_urls

--- evrgreen/views.py
+++ evrgreen/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def home(request):
+   queryset = Product.objects.all()
+   context={'product':queryset}
+   return render(request,'base.html',context)
